636add4653b7db387de738375c867c37ad1bd8a6
Élie Gouzien <elie.gouzien@alice-bob.com>
2024-05-31 20:39:23 +0200

    Reorganize examples.

--- examples/elliptic_log.rs
+++ examples/elliptic_log.rs
@@ -6,29 +6,8 @@
 //! Estimate the ressources required for Elliptic Curve Cryptography (ECC) on a cat-based quantum
 //! processor.
 //!
-//! Author: Mathias Soeken
-//!
 //! Based on É. Gouzien et al.'s article (<https://arxiv.org/abs/2302.06639>) and code
 //! (<https://github.com/ElieGouzien/elliptic_log_cat/tree/master>).
-//!
-//! <b>Inputs:</b><br>
-//! <pre>
-//! - Qubit parameters (qubit.rs):
-//!      * k₁_k₂ = ratio one photon/two photon losses (1e-5 hardcoded)
-//! - Gates parameters (factories.rs):
-//!      * t = single physical gate time (100 ns hardcoded). Same value assumed for state preparation, measurement, CNOT and Toffoli
-//!      * gate_time ∝ time steps (89.2 time steps hardcoded)
-//! - Repetition code parameters (code.rs):
-//!      * (κ₁/κ₂)_th: fault tolerance threshold (0.013 hardcoded)
-//! </pre>
-//! <b>Outputs:</b>
-//! <pre>
-//! - # of physical cat qubits
-//! - Runtime
-//! - Total error probability
-//! - Repetition code distance & # of photons
-//! - Ffraction of qubits assigned to the magic state factory
-//! </pre>
 
 use std::rc::Rc;
 
@@ -68,24 +47,6 @@ fn main() -> Result<(), anyhow::Error> {
     for r in results {
         println!("{}", AliceAndBobEstimates::from(r));
     }
-    println!("----------------------------------------");
-
-    // Resource estimation from Q#
-    // ---------------------------
-
-    let filename = format!("{}/qsharp/Adder.qs", env!("CARGO_MANIFEST_DIR"));
-
-    let qubit = CatQubit::new();
-    let qec = RepetitionCode::new();
-    let builder = ToffoliBuilder::default();
-    let overhead = Rc::new(LogicalCounts::from_qsharp(filename).map_err(anyhow::Error::msg)?);
-    let budget = ErrorBudget::new(0.001 * 0.5, 0.001 * 0.5, 0.0);
-
-    let estimation =
-        PhysicalResourceEstimation::new(qec, Rc::new(qubit), builder, overhead, budget);
-    let result: AliceAndBobEstimates = estimation.estimate()?.into();
-    println!("Resource estimate from Q# code (ripple-carry adder):");
-    println!("{result}");
 
     Ok(())
 }
---
+++ examples/from_qsharp.rs
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation.
+// Copyright (c) Alice & Bob
+// Licensed under the MIT License.
+
+#![warn(missing_docs)]
+//! Estimate the ressources required for an adder, the adder being specified by a Q# file.
+
+use std::rc::Rc;
+
+use qsharp_alice_bob_resource_estimator::{RepetitionCode, LogicalCounts, AliceAndBobEstimates, ToffoliBuilder, CatQubit};
+use resource_estimator::estimates::{ErrorBudget, PhysicalResourceEstimation};
+
+fn main() -> Result<(), anyhow::Error> {
+    // Resource estimation from Q#
+    // ---------------------------
+
+    let filename = format!("{}/qsharp/Adder.qs", env!("CARGO_MANIFEST_DIR"));
+
+    let qubit = CatQubit::new();
+    let qec = RepetitionCode::new();
+    let builder = ToffoliBuilder::default();
+    let overhead = Rc::new(LogicalCounts::from_qsharp(filename).map_err(anyhow::Error::msg)?);
+    let budget = ErrorBudget::new(0.001 * 0.5, 0.001 * 0.5, 0.0);
+
+    let estimation =
+        PhysicalResourceEstimation::new(qec, Rc::new(qubit), builder, overhead, budget);
+    let result: AliceAndBobEstimates = estimation.estimate()?.into();
+    println!("Resource estimate from Q# code (ripple-carry adder):");
+    println!("{result}");
+
+    Ok(())
+}
